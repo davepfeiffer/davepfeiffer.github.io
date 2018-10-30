76e17fd73b9baa3de3990c28c2e81b2995bde977
David Pfeiffer <davepfeifferer@gmail.com>
2018-10-29 22:02:22 -0400

    strip down

---
+++ Makefile
@@ -0,0 +1,7 @@
+
+SRC=./src
+BIN=./articles
+$(BIN)/2017-08-22-debounce.html: $(SRC)/2017-08-22-debounce.md
+	 pandoc -s -t html5 --output $@ --css ./article.css $<
+
+all:  $(BIN)/2017-08-22-debounce.html
---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+
+ARTICLE_BASE=articles
+SITE_BASE=dmpfeiffer.com
+SRC=src
+ALL="all: "
+
+cp index.template index.md
+cp Makefile.template Makefile
+
+for ARTICLE in $(ls $SRC | grep -e "^[^_]")
+do
+  echo "* [${ARTICLE%.*}]($SITE_BASE/$ARTICLE_BASE/${ARTICLE%.*}.html)" >> index.md
+  printf "\$(BIN)/${ARTICLE%.*}.html: \$(SRC)/$ARTICLE\n" >> Makefile
+  printf "\t pandoc -s -t html5 --output \$@ --css ./article.css \$<\n\n" >> Makefile
+  ALL="${ALL} \$(BIN)/${ARTICLE%.*}.html"
+done
+
+echo "$ALL" >> Makefile
+
+pandoc \
+  -s -t html5 \
+  --output index.html \
+  --css ./article.css \
+  index.md
+
+make all
